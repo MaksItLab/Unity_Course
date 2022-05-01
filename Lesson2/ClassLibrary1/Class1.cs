ce4cfe876fe80f032afdbb56c6aca44f5125923a
Maksim <maksim.ardintseb@mail.ru>
2022-05-01 23:29:23 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/ClassLibrary1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studing.Utils
+{
+    public class Helpers
+    {
+        public static void Print(string str)
+        {
+            Console.Write($"{str}");
+        }
+        public static void Print(int number)
+        {
+            Console.Write(number);
+        }
+        public static void PrintLine(string str)
+        {
+            Console.WriteLine($"{str}");
+        }
+        public static void PrintLine(int number)
+        {
+            Console.WriteLine(number);
+        }
+        public static void PrintLine(long number)
+        {
+            Console.WriteLine(number);
+        }
+
+
+        public static void Pause()
+        {
+            Console.ReadLine();
+        }
+
+        public static string Enter()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
