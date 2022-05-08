e3b5b3943c3ea117f7aec410f57cf1757f4119e1
Maksim <maksim.ardintseb@mail.ru>
2022-05-08 19:09:32 +0300

    дз 3

---
+++ Lesson_3/LibraryHelp/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LibraryHelp
+{
+    public class Helpers
+    {
+        public static void Pause()
+        {
+            Console.ReadLine();
+        }
+
+        public static string Enter()
+        {
+            return Console.ReadLine();
+        }
+
+        public static void Out(object x)
+        {
+            Console.Write(x);
+        }
+
+        public static void OutLine(object x)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
